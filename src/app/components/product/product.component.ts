ed50ff5783d7bf885b70c8d667741e999556f52d
Cagdas <74134486+cagdask61@users.noreply.github.com>
2021-03-11 21:50:23 +0300

    21.49 -- 11/372021 -- Front-End -- Angular

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,18 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1:any = {productId:1, productName:'Bardak', categoryId:1, unitPrice:20}
-  product2:any = {productId:2, productName:'Mouse', categoryId:2, unitPrice:20}
-  product3:any = {productId:3, productName:'Camera', categoryId:3, unitPrice:20}
-  product4:any = {productId:4, productName:'Klavye', categoryId:4, unitPrice:20}
-  product5:any = {productId:5, productName:'Telefon', categoryId:5, unitPrice:20}
+  products:Product[] = [];
+  dataLoaded=false;
   
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+  constructor(private productService:ProductService) {
 
-
-  constructor() { }
+   }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+
+  getProducts(){
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded=true;
+    })
+  }
 }
